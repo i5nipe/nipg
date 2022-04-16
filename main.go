92bdb2f8ed8db18c097ab734170cb9e95549f06a
i5nipe <thalles.isnipe@gmail.com>
2022-04-16 15:42:03 -0300

    Criando Função para rolar os dados

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 func main() {
 
 	http.HandleFunc("/", HelloHandler)
 
-	http.HandleFunc("/dado", RollDice)
+	http.HandleFunc("/api/dado", RollDicehttp)
 
 	fmt.Println("Server started at port 1313")
 	http.ListenAndServe(":1313", nil)
@@ -20,16 +22,16 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello Word")
 }
 
-func RollDice(w http.ResponseWriter, r *http.Request) {
+func RollDicehttp(w http.ResponseWriter, r *http.Request) {
 	var faces int
 
-	keys, ok := r.URL.Query()["faces"]
+	keys, ok := r.URL.Query()["lados"]
 
 	if ok {
 		faces_str := keys[0]
 		faces, err := strconv.Atoi(faces_str)
 		if err != nil {
-			fmt.Println("'?faces' não é um Inteiro")
+			fmt.Println("'?lados' não é um Inteiro")
 			faces = 20
 		}
 
@@ -38,4 +40,9 @@ func RollDice(w http.ResponseWriter, r *http.Request) {
 		faces = 20
 		fmt.Fprintln(w, faces)
 	}
+}
+
+func RollDice(faces int) {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(faces) + 1
 }
