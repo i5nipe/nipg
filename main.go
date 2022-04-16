a6f11c612f3ab170c3f54b9f8f0c7ddee59a2660
i5nipe <thalles.isnipe@gmail.com>
2022-04-15 22:02:12 -0300

    Inicio função rolldice

--- main.go
+++ main.go
@@ -9,11 +9,26 @@ func main() {
 
 	http.HandleFunc("/", HelloHandler)
 
+	http.HandlerFunc("/dado", RollDice)
+
 	fmt.Println("Server started at port 1313")
 	http.ListenAndServe(":1313", nil)
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-
 	fmt.Fprintln(w, "Hello Word")
+}
+
+func RollDice(w http.ResponseWriter, r *http.Request) {
+
+	keys, ok := r.URL.Query()["faces"]
+
+	faces := 20
+
+	if ok {
+		faces = keys[0]
+	}
+
+	fmt.Fprintln(w, faces)
+
 }
