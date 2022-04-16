87db31242200e6494c050ad99879d08fead263fb
i5nipe <thalles.isnipe@gmail.com>
2022-04-15 22:09:41 -0300

    Dezenas de error para fazer o dado rolar

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 
 	http.HandleFunc("/", HelloHandler)
 
-	http.HandlerFunc("/dado", RollDice)
+	http.HandleFunc("/dado", RollDice)
 
 	fmt.Println("Server started at port 1313")
 	http.ListenAndServe(":1313", nil)
@@ -21,12 +22,14 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 
 func RollDice(w http.ResponseWriter, r *http.Request) {
 
-	keys, ok := r.URL.Query()["faces"]
+	keys, _ := r.URL.Query()["faces"]
 
 	faces := 20
-
-	if ok {
-		faces = keys[0]
+	faces_str := keys[0]
+	faces, err := strconv.Atoi(faces_str)
+	if err != nil {
+		fmt.Println("'?faces' não é um Inteiro")
+		faces = 20
 	}
 
 	fmt.Fprintln(w, faces)
