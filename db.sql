436e4b923b2965c9a75cd5b89c5f07ade20c6b00
i5nipe <thalles.isnipe@gmail.com>
2022-04-16 17:32:25 -0300

    Adicionado db.sql

---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `Users` (
+	`id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+	`user` varchar(30) NOT NULL,
+	`password` varchar(30) NOT NULL,
+	`role` varchar(30) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
